c8fe4c8134ae20864603b93f8d58392a66635400
mingu kang <97663670+mingu4u@users.noreply.github.com>
2025-03-31 23:45:52 +0900

    Add files via upload

--- binance_future.py
+++ binance_future.py
@@ -5663,8 +5663,8 @@ def ai_trading():
         # Capture chart with retry logic
         chart_image, signals_analysis, saved_file_path = capture_tradingview_chart_with_retry(
             chart_processor=chart_processor, 
-            save_image=False, 
-            debug=False,
+            save_image=True, 
+            debug=True,
             max_retries=3,
             page_load_timeout=40
         )
@@ -5949,11 +5949,11 @@ def ai_trading():
                 price_change_pct = (entry_price - signal_price) / signal_price * 100
             
             # 수정: Volume Oscillator 조건 완화 - 강한 신호가 있을 경우 음수도 허용
-            # strong_signals = blackflag_valid and utbot_valid and trend_strength_result.get("long_trend_strong", False)
-            volume_valid = signals_data.get("VolumeOsc_Current", -999) > -15
-            if signals_data.get("VolumeOsc_Current", -999) <= -15:
-                volume_valid = False
-
+            strong_signals = blackflag_valid and utbot_valid and trend_strength_result.get("long_trend_strong", False)
+            volume_valid = signals_data.get("VolumeOsc_Current", -999) > 0 or (
+                strong_signals and signals_data.get("VolumeOsc_Current", -999) > -15
+            )
+            
             trend_valid = trend_strength_result.get("long_trend_strong", False)
             
             # 가격 변화 조건 (2% 이상 상승하면 진입하지 않음)
@@ -6062,11 +6062,10 @@ def ai_trading():
                 price_change_pct = (signal_price - entry_price) / signal_price * 100
             
             # 수정: Volume Oscillator 조건 완화 - 강한 신호가 있을 경우 음수도 허용
-            # strong_signals = blackflag_valid and utbot_valid and trend_strength_result.get("short_trend_strong", False)
-            volume_valid = signals_data.get("VolumeOsc_Current", -999) > -15
-            if signals_data.get("VolumeOsc_Current", -999) <= -15:
-                volume_valid = False
-            
+            strong_signals = blackflag_valid and utbot_valid and trend_strength_result.get("short_trend_strong", False)
+            volume_valid = signals_data.get("VolumeOsc_Current", -999) > 0 or (
+                strong_signals and signals_data.get("VolumeOsc_Current", -999) > -15
+            )
             
             trend_valid = trend_strength_result.get("short_trend_strong", False)
             
